cce5e4feb3100fc6afe2286cd56d0ae11afe7aa6
Ismail Vega <ivmena@asapp.com>
2023-03-19 01:49:29 -0300

    build(FE): correct path to public
    Signed-off-by: Ismail Vega <ivmena@asapp.com>

--- app.js
+++ app.js
@@ -33,17 +33,6 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/", router);
 
-if (process.env.NODE_ENV === "production") {
-  // Express  will serve up production assets
-  // like our main.js file, or main.css file!
-  app.use(express.static("client/dist", options)); //Finally - Express look if the request is for an asset
-  // Express will serve up the index.html file
-  // if it doesn't recognize the route
-  const path = require("path");
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
-  });
-}
+app.use(express.static("public", options));
 
 module.exports = app;
