4b55c1f251844715f92c6fe7ffae862bcba12de7
Ismail Vega <ivmena@asapp.com>
2023-03-20 01:55:55 -0300

    test(WebContainer): add changes to the api
    Signed-off-by: Ismail Vega <ivmena@asapp.com>

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 const express = require("express");
 const app = express();
-const path = require("path");
-const cors = require("cors");
 const router = require("./routes");
 
 // #############################################################################
@@ -27,7 +25,6 @@ var options = {
   redirect: false,
 };
 
-app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,4 @@
 const router = require("express").Router();
-const userController = require("../controllers/users");
 const dbController = require("../controllers/collections");
 
 // Create or Update a collection
@@ -13,14 +12,5 @@ router.route("/:col/:key").get(dbController.getCollection);
 
 // Get a full listing of collections
 router.route("/:col").get(dbController.getCollections);
-
-// Handle external authentication
-router.route("/login").post(userController.handleLogin);
-
-// Handle Logout
-router.route("/logout").get(userController.handleLogOut);
-
-// Get current user data
-router.route("/current").get(userController.getUser);
 
 module.exports = router;
